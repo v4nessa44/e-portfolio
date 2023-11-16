619fa9873e3031b125ad630e611765f51f59152e
v4nessa04 <v4nessa04@gmail.com>
2023-11-15 21:46:59 -0500

    added Browser Routes

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import AboutUs from './pages/AboutUs'
 import Portfolio from './pages/Portfolio';
 import Conctact from './pages/Conctact';
 import Resume from './pages/Resume';
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 
 function App() {
   return (
     <div>
+       <BrowserRouter >
       <Routes>
 
         <Route path="/e-portfolio" element={<AboutUs />} />
@@ -18,7 +19,7 @@ function App() {
         <Route path="/contact" element={<Conctact />} />
     <Route path="/resume" element={<Resume />} />
       </Routes>
-
+      </BrowserRouter>
     </div>
   );
 }
